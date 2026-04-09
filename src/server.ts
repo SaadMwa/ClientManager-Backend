29b92e8c00a78fd041231b17727464111c410599
Saad <mwasaad47@gmail.com>
2026-04-09 15:34:13 +0500

    vercel issue fixed

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,14 @@ import { app, connectToDatabase } from "./app.js";
 // Database connection and server start
 const Port = process.env.PORT || 5000;
 
-connectToDatabase()
-  .then(() => {
-    console.log("âœ… Connected to MongoDB");
-    app.listen(Port, () => {
-      console.log(`ðŸš€ Server is running on port ${Port}`);
-    });
-  })
-  .catch((err) => {
-    console.error("âŒ Failed to connect to MongoDB", err);
-  });
+// connectToDatabase()
+//   .then(() => {
+//     console.log("âœ… Connected to MongoDB");
+//     app.listen(Port, () => {
+//       console.log(`ðŸš€ Server is running on port ${Port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("âŒ Failed to connect to MongoDB", err);
+//   });
+  export default app;
